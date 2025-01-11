fad2ed9e5d53a63be091b2e08fa9d668e8e0e6f3
Manish <m4manishp4prasad@gmail.com>
2025-01-11 16:01:50 +0530

    Initial Commit

---
+++ nectar/nectarApp.swift
@@ -0,0 +1,17 @@
+//
+//  nectarApp.swift
+//  nectar
+//
+//  Created by Abhijeet Rai on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct nectarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nectarTests/nectarTests.swift
@@ -0,0 +1,16 @@
+//
+//  nectarTests.swift
+//  nectarTests
+//
+//  Created by Abhijeet Rai on 11/01/25.
+//
+
+import Testing
+
+struct nectarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
